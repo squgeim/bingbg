b9e8530903009ccd99f510a656313b30dd017b9b
Shreya Dahal <shreyadahal@gmail.com>
2014-05-17 09:59:06 +0545

    Initial commit

---
+++ bingbg.py
@@ -0,0 +1,55 @@
+
+import re, urllib, os
+from config import conf
+
+def getimg(FILE):
+  f=open(FILE,'r')
+  cont=f.read()
+  os.remove('.temp')
+  s=re.search(r"g_img={\w*url\:\'(.*\.jpg)\'.*id\:\'bgDiv\',",cont)
+  if not s:
+    print "Sorry, there was some problems, please try again later."
+    return
+  img='http://bing.com'
+  img+=s.group(1)
+  try:
+    img=getres(img)
+  except:
+    return
+  name=img.split('/')[-1]
+  path=checkpath(name)
+  if path:
+    print "Downloading",name
+    urllib.urlretrieve(img,path)
+    print "Done. Go check it out."
+
+
+def getres(img):
+  s=re.search(r'_(\d\d\d\d*x\d\d\d\d*)',img)
+  if not s:
+    print "There was an error"
+    raise
+  img2=img.replace(s.group(1),conf['res'])
+  try:
+    f=urllib.urlopen(img2)
+    return img2
+  except:
+    return img
+
+def checkpath(name):
+  if os.path.isdir(conf['des']):
+    if os.path.isfile(conf['des']+name):
+      print "Nothing new. See ya later."
+      return False
+    else:
+      return conf['des']+name
+  else:
+    return '~/Pictures/'+name
+
+if __name__=='__main__':
+  print "Asking Bing if it has anything new for me..."
+  try:
+    urllib.urlretrieve('http://bing.com','.temp')
+  except:
+    print "Sorry man, couldn't reach Bing. Something wrong with your connection?"
+  getimg('.temp')
---
+++ config.py
@@ -0,0 +1,11 @@
+#!/bin/python
+
+conf = {
+	# The resolution to download (if the given resolution is not found, 
+	# your current desktop resolution will be used):
+	'res':'1920x1200',
+
+	# The destination to download to (note the / at the end). Make sure you 
+	# have write permissions to that directory:
+	'des':'/media/0xsda5/Media/Wallpapers/Bing/'
+}
