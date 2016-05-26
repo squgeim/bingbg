1f0b8d5ed76925e24759c01d7cd936985831c1d8
Shreya Dahal <shreyadahal@gmail.com>
2016-05-26 18:27:04 +0545

    Using DOM based scraping now
    Uses BeautifulSoup, Selenium and CSSUtils
    TODO: get PhantomJS to work

--- bingbg.py
+++ bingbg.py
@@ -1,57 +1,85 @@
 #!/usr/bin/python2
 
-import re, urllib, os
+import re, os, urllib
 from config import conf
+from bs4 import BeautifulSoup
+import cssutils
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
 
-def getimg(FILE):
-  f=open(FILE,'r')
-  cont=f.read()
-  os.remove('.temp')
-  s=re.search(r"g_img={\w*url\:\'(.*\.jpg)\'.*id\:\'bgDiv\',",cont)
-  if not s:
-    print "Sorry, there were some problems, please try again later."
-    return
-  img='http://bing.com'
-  img+=s.group(1)
-  try:
-    img=getres(img)
-  except:
-    return
-  name=img.split('/')[-1]
-  path=checkpath(name)
-  if path:
-    print "Downloading",name
-    urllib.urlretrieve(img,path)
-    print "Done. Go check it out."
-
-def getres(img):
-  s=re.search(r'_(\d\d\d\d*x\d\d\d\d*)',img)
-  if not s:
-    print "There was an error"
-    raise
-  img2=img.replace(s.group(1),conf['res'])
-  try:
-    f=urllib.urlopen(img2)
-    return img2
-  except:
-    return img
-
-def checkpath(name):
-  if os.path.isdir(conf['des']):
-    if os.path.isfile(conf['des']+name):
-      print "Nothing new. See ya later."
+class bingBg():
+  
+  def __init__(self):
+    html = self.get_html()
+    self.img_url = self.get_image_url(html)
+    self.download_dir = self.get_directory()
+  
+  def has_new(self):
+    if os.path.isfile( self.get_directory() + self.get_image_name() ):
       return False
     else:
-      return conf['des']+name
-  else:
-    print "The given directory doesn't exist or you do no have the write permissions."
-    print "Downloading to your home/Pictures directory."
-    return '~/Pictures/'+name
-
-if __name__=='__main__':
+      return True
+      
+  def download_image(self):
+    urllib.urlretrieve( self.img_url, self.download_dir + self.get_image_name() )
+    
+  def get_html(self):
+    '''
+      TODO: get phantomjs to work
+    '''
+    wd = webdriver.Firefox()
+    #wd = webdriver.PhantomJS()
+    wd.get('https://bing.com')
+    WebDriverWait(wd, 5)
+    temp = wd.page_source
+    wd.quit()
+    return temp
+    
+  def get_image_url(self, html):
+    soup = BeautifulSoup(html, 'html.parser')
+    bgDiv = soup.find('div', {'id':'bgDiv'})
+    divStyle = cssutils.parseString('.div {' + str(bgDiv['style']) + '}')
+    image = divStyle.cssRules[0].style.backgroundImage
+    s = re.search(r'url\((.*\.jpg)\)', image)
+    if not s:
+      raise Exception('Couldn\'t parse html.')
+    url = s.group(1)
+    try:
+      url = self.getres(url)
+    except:
+      raise Exception('Can\'t parse filename.')
+    return url
+    
+  def get_image_name(self):
+    return self.img_url.split('/')[-1]
+    
+  def getres(self, img):
+    s=re.search(r'_(\d\d\d\d*x\d\d\d\d*)',img)
+    if not s:
+      raise
+    img2=img.replace(s.group(1),conf['res'])
+    try:
+      f=urllib.urlopen(img2)
+      return img2
+    except:
+      return img
+      
+  def get_directory(self):
+    if os.path.isdir(conf['des']):
+      return conf['des']
+    else:
+      return '~/Pictures/'
+      
+if __name__ == '__main__':
   print "Asking Bing if it has anything new for me..."
   try:
-    urllib.urlretrieve('http://bing.com','.temp')
-    getimg('.temp')
+    app = bingBg()
+    if not app.has_new():
+      print 'Nothing new. See ya later.'
+    else:
+      print 'Downloading ' + app.get_image_name() + '...'
+      app.download_image()
+      print 'Done. Go check it out.'
   except:
-    print "Sorry man, couldn't reach Bing. Something wrong with your connection?"
+    print 'There was a problem reaching Bing.'
+      
